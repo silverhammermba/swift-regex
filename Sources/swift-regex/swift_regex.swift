892d6227d2553df2bc07c747e9403ad12ff5eef3
Max <silverhammermba@gmail.com>
2020-09-26 16:21:27 -0400

    add pattern matching stuff

--- Sources/swift-regex/swift_regex.swift
+++ Sources/swift-regex/swift_regex.swift
@@ -1,11 +1,13 @@
 import Foundation
 
-infix operator ~: AdditionPrecedence
+infix operator ~: ComparisonPrecedence
+infix operator ~?: ComparisonPrecedence
 
 public struct Regexp {
-    public struct Match {
+    public struct Match: Sequence, IteratorProtocol {
         private let string: String
         private let result: NSTextCheckingResult
+        private var index = 0
 
         public var groups: Int { return result.numberOfRanges - 1 }
         public var substring: Substring { return string[Range(result.range, in: string)!] }
@@ -15,8 +17,18 @@ public struct Regexp {
             self.result = result
         }
 
-        public subscript(_ index: Int) -> Substring {
-            return string[Range(result.range(at: index + 1), in: string)!]
+        public subscript(_ index: Int) -> Substring?! {
+            // TODO: should negative do something?
+            guard index >= 0 && index < groups else { return nil }
+            let range = result.range(at: index + 1)
+            guard range.location != NSNotFound else { return .some(nil) }
+            return string[Range(range, in: string)!]
+        }
+        public mutating func next() -> Substring?? {
+            guard index < groups else { return nil }
+            let sub = self[index]
+            index += 1
+            return sub
         }
     }
 
@@ -26,8 +38,56 @@ public struct Regexp {
         regexp = try NSRegularExpression(pattern: pattern, options: options)
     }
 
-    public static func ~ (left: Regexp, right: String) -> [Match] {
-        return left.regexp.matches(in: right, range: NSRange(right.startIndex..., in: right)).map { Match($0, in: right) }
+    public static func ~= (lhs: Regexp, rhs: String) -> Bool {
+        return lhs.regexp.firstMatch(in: rhs, range: NSRange(rhs.startIndex..., in: rhs)) != nil
+    }
+
+    public static func ~ (lhs: Regexp, rhs: String) -> [Match] {
+        return lhs.regexp.matches(in: rhs, range: NSRange(rhs.startIndex..., in: rhs)).map { Match($0, in: rhs) }
+    }
+
+    public static func ~? (lhs: Regexp, rhs: String) -> Match? {
+        return lhs.regexp.firstMatch(in: rhs, range: NSRange(rhs.startIndex..., in: rhs)).map { Match($0, in: rhs) }
     }
 }
 
+extension Regexp.Match {
+    public func t1(_ start: Int = 0) -> Substring? {
+        return groups > start + 0 ? self[start + 0] : nil
+    }
+
+    public func t2(_ start: Int = 0) -> (Substring?, Substring?) {
+        let t = t1(start)
+        return (t, groups > start + 1 ? self[start + 1] : nil)
+    }
+
+    public func t3(_ start: Int = 0) -> (Substring?, Substring?, Substring?) {
+        let t = t2(start)
+        return (t.0, t.1, groups > start + 2 ? self[start + 2] : nil)
+    }
+
+    public func t4(_ start: Int = 0) -> (Substring?, Substring?, Substring?, Substring?) {
+        let t = t3(start)
+        return (t.0, t.1, t.2, groups > start + 3 ? self[start + 3] : nil)
+    }
+
+    public func t5(_ start: Int = 0) -> (Substring?, Substring?, Substring?, Substring?, Substring?) {
+        let t = t4(start)
+        return (t.0, t.1, t.2, t.3, groups > start + 4 ? self[start + 4] : nil)
+    }
+
+    public func t6(_ start: Int = 0) -> (Substring?, Substring?, Substring?, Substring?, Substring?, Substring?) {
+        let t = t5(start)
+        return (t.0, t.1, t.2, t.3, t.4, groups > start + 5 ? self[start + 5] : nil)
+    }
+
+    public func t7(_ start: Int = 0) -> (Substring?, Substring?, Substring?, Substring?, Substring?, Substring?, Substring?) {
+        let t = t6(start)
+        return (t.0, t.1, t.2, t.3, t.4, t.5, groups > start + 6 ? self[start + 6] : nil)
+    }
+
+    public func t8(_ start: Int = 0) -> (Substring?, Substring?, Substring?, Substring?, Substring?, Substring?, Substring?, Substring?) {
+        let t = t7(start)
+        return (t.0, t.1, t.2, t.3, t.4, t.5, t.6, groups > start + 7 ? self[start + 7] : nil)
+    }
+}
--- Tests/swift-regexTests/swift_regexTests.swift
+++ Tests/swift-regexTests/swift_regexTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import swift_regex
 
 final class swift_regexTests: XCTestCase {
-    func testExample() {
+    func testSimple() {
         let r: Regexp! = try? Regexp(#"cat"#)
         XCTAssertNotNil(r)
         let m = r ~ "cattle"
@@ -11,7 +11,100 @@ final class swift_regexTests: XCTestCase {
         XCTAssertEqual(m.first!.substring, "cat")
     }
 
+    func testGroups() {
+        let r = try! Regexp(#"(a|(b+)(c*))"#)
+
+        let m = (r ~ "apple").first!
+        XCTAssertEqual(m.substring, "a")
+        XCTAssertEqual(m.map { $0 }, ["a", nil, nil])
+
+        let m2 = r ~ "cabby"
+        XCTAssertEqual(m2.count, 2)
+        XCTAssertEqual(m2[1].map { $0 }, ["bb", "bb", ""])
+    }
+
+    func testSwitch() {
+        let s = "hey there, buddy!"
+
+        // match against multiple patterns
+        switch s {
+            case try! Regexp("foo"):
+                XCTFail()
+            case try! Regexp("here,"):
+                break
+            case try! Regexp("buddy"):
+                XCTFail()
+            default:
+                XCTFail()
+        }
+
+        // or make one pattern
+        let reg = try! Regexp(#"((\w+(!|\?))|(\d{3}))"#)
+
+        // and check how it matched
+        switch (reg ~? s)?.t2() {
+            case (_, "buddy!")?:
+                break
+            default:
+                XCTFail()
+        }
+
+        switch (reg ~? "number 1234 cool?")?.t3(1) {
+            case (_?, _, nil)?:
+                XCTFail()
+            case (nil, _, let digits?)?:
+                XCTAssertEqual(digits, "123")
+            default:
+                XCTFail()
+        }
+    }
+
+    enum ParserError: Error {
+        case fuck
+    }
+
+    func testTime() {
+        func comp2t(_ neg: Bool, _ h: Int, _ m: Int, _ s: Int) -> TimeInterval {
+            let int = TimeInterval(h * 3600 + m * 60 + s)
+            return neg ? -int : int
+        }
+
+        func parse(_ s: String) throws -> TimeInterval {
+            // parse some time offset like -04:03:02 with 0-3 components
+            let reg = try! Regexp(#"^(|(-)?(\d+)(:(\d+))?(:(\d+))?)$"#)
+
+            switch (reg ~? s)?.t7() {
+                case (_, nil, nil, nil, nil, nil, nil)?:
+                    return 0
+                case (_, let s, let c1?, _, let c2?, _, let c3?)?:
+                    return comp2t(s != nil, Int(c1)!, Int(c2)!, Int(c3)!)
+                case (_, let s, let c1?, _, let c2?, _, _)?:
+                    return comp2t(s != nil, 0, Int(c1)!, Int(c2)!)
+                case (_, let s, let c1?, _, _, _, _)?:
+                    return comp2t(s != nil, 0, 0, Int(c1)!)
+                default:
+                    throw ParserError.fuck
+            }
+        }
+
+        XCTAssertEqual(try? parse(""), 0)
+        XCTAssertEqual(try? parse("210"), 210)
+        XCTAssertEqual(try? parse("3:2"), 3*60+2)
+        XCTAssertEqual(try? parse("-01:02"), -(60+2))
+        XCTAssertEqual(try? parse("-1:02:03"), -(3600+2*60+3))
+        XCTAssertEqual(try? parse("04:02:03"), 4*3600+2*60+3)
+
+        XCTAssertThrowsError(try parse("cat"))
+        XCTAssertThrowsError(try parse("1:"))
+        XCTAssertThrowsError(try parse("1:-2"))
+        XCTAssertThrowsError(try parse("1:2:a"))
+        XCTAssertThrowsError(try parse("1:2:3:a"))
+        XCTAssertThrowsError(try parse("1:2:5:6"))
+    }
+
     static var allTests = [
-        ("testExample", testExample),
+        ("testSimple", testSimple),
+        ("testGroups", testGroups),
+        ("testSwitch", testSwitch),
     ]
 }
